b26151fae21ecc526bf0e6763caabfd9e2af28c8
hfccr <hfccr@outlook.com>
2025-09-27 06:25:00 +0530

    Refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// import { Inter, JetBrains_Mono } from "next/font/google";
 import { Web3Provider } from "@/provider/web3-provider";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkTheme } from "@/utils/theme";
@@ -8,16 +7,6 @@ import "@fontsource/asimovian";
 import "@fontsource/ibm-plex-sans";
 import "@fontsource-variable/open-sans";
 import "./globals.css";
-
-// const inter = Inter({
-//   subsets: ["latin"],
-//   variable: "--font-inter",
-// });
-
-// const jetBrainsMono = JetBrains_Mono({
-//   subsets: ["latin"],
-//   variable: "--font-jetbrains-mono",
-// });
 
 export const metadata: Metadata = {
   title: "Juni",
